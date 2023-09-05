4478e44a6282809621c0a9014ed49fe8c0509229
tast <ioae7979@gmail.com>
2023-09-05 22:00:48 +0900

    test.2 commit

--- src/asdasd.java
+++ src/asdasd.java
@@ -1,6 +1,6 @@
 public class asdasd {
     public static void main(String[] args) {
         System.out.println("나는");
-        System.out.println("빡빡이다");
+        System.out.println("박박이다");
     }
 }
